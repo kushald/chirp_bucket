6f23b421942b39801ff97d93e036a2923ad59abc
Kushal Dongre <kushz19@gmail.com>
2013-12-04 18:09:47 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chirpbucket::Application.config.secret_key_base = '0595b7c9018083a28df77062bf818eb86bd2557f55a683071c8049c038abb1663d54870f82e2a18b4355f853494ac0529de49b5d20335bd46f87bbe94f030297'
